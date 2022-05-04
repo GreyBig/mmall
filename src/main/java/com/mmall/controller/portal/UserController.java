8ee372789743d2d9357acd714c2d749b3ed744ba
gray <gray@dd.com>
2022-05-04 20:25:32 +0800

    用户模块-登录功能开发

---
+++ src/main/java/com/mmall/controller/portal/UserController.java
@@ -0,0 +1,26 @@
+package com.mmall.controller.portal;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+import javax.servlet.http.HttpSession;
+
+@Controller
+@RequestMapping("/user/") // 这个类所有的方法访问路径前面都有/user/
+public class UserController {
+
+    /**
+     * 用户登录
+     * @param username
+     * @param password
+     * @param session
+     * @return
+     */
+    @RequestMapping(value = "login.do", method = RequestMethod.POST)
+    @ResponseBody
+    public Object login(String username, String password, HttpSession session) {
+        return null;
+    }
+}
